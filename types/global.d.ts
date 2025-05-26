5ec0a1eed477193a8d42412c414081bccb13ae52
galaxysteven111 <galaxysteven85@gmail.com>
2025-05-26 13:11:52 +0800

    Update global.d.ts

--- types/global.d.ts
+++ types/global.d.ts
@@ -4,7 +4,12 @@ declare global {
   interface Window {
     solana?: {
       isPhantom?: boolean
-      connect: (options?: any) => Promise<{ publicKey: { toString(): string } }>
+      connect: (options?: any) => Promise<{
+        publicKey: {
+          toString(): string
+          toBase58(): string
+        }
+      }>
       publicKey?: {
         toBase58(): string
       }
